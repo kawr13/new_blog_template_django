c94aaaa73a835faae31c2bcc7f1ac176eff1de3c
kawr <kawr@gmail.com>
2023-11-12 02:03:56 +0500

    Create edit models comment and Post

--- postapp/models.py
+++ postapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 
@@ -10,11 +10,23 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField(null=True, blank=True)
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-publish',) # Делаем сортировку по дате публикации
+        indexes = (
+            models.Index(fields=['-publish']), # Добавляем индексы для ускорения работы с базой данных
+        )
+    def __str__(self):
+        return self.name
