b947ba4c35c23d2ed3dca696914c3e00d1ad0248
kawr <kawr@gmail.com>
2023-11-12 01:23:57 +0500

    Create Post models

--- postapp/models.py
+++ postapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
