3974c57f6dcac5dfd3ef05fe0e3c9e275e2e50f6
kawr <kawr@gmail.com>
2023-11-12 01:36:54 +0500

    Create Comments models

--- postapp/models.py
+++ postapp/models.py
@@ -11,3 +11,10 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
