56b1f62b64b53415d2272dafc941adae78853fdd
kawr <kawr@gmail.com>
2023-11-12 02:13:43 +0500

    Add models Post: authors

--- postapp/models.py
+++ postapp/models.py
@@ -1,14 +1,30 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+        ARCHIVED = 'AC'
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ('-publish',) # Делаем сортировку по дате публикации
+        indexes = (
+            models.Index(fields=['-publish']), # Добавляем индексы для ускорения работы с базой данных
+        )
 
     def __str__(self):
         return self.title
