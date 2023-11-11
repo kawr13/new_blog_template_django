5ecd1a195cd2e903ca219467ced46b709ff6830a
kawr <kawr@gmail.com>
2023-11-12 02:19:20 +0500

    realis migrate

--- postapp/admin.py
+++ postapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Post, Comment
 # Register your models here.
+
+
+class CommentTabularInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created_at', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('-publish',)
+    inlines = [CommentTabularInline]
